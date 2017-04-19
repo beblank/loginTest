b42c2fc9ea9699ebcdab2651b3de4e2466e986f6
beblank <hadiwijaya.aditya@gmail.com>
2017-04-19 21:02:56 +0700

    login view inject

--- app/src/main/java/com/example/adit/mobiletagloc/LoginActivity.java
+++ app/src/main/java/com/example/adit/mobiletagloc/LoginActivity.java
@@ -2,10 +2,19 @@ package com.example.adit.mobiletagloc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.example.adit.mobiletagloc.base.App;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.usernameEdit)
+    EditText usernameEdit;
+
+    @BindView(R.id.passwordEdit)
+    EditText passwordEdit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
