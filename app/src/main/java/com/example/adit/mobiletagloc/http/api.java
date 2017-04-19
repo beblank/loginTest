de5e74e39377e5de9d1b1ce293bf23ca20f596ac
beblank <hadiwijaya.aditya@gmail.com>
2017-04-19 20:47:29 +0700

    prepare for api service

---
+++ app/src/main/java/com/example/adit/mobiletagloc/http/api.java
@@ -0,0 +1,7 @@
+package com.example.adit.mobiletagloc.http;
+
+/**
+ * Created by adit on 4/19/2017.
+ */
+public class api {
+}
---
+++ app/src/main/java/com/example/adit/mobiletagloc/http/login/LoginApi.java
@@ -0,0 +1,7 @@
+package com.example.adit.mobiletagloc.http.login;
+
+/**
+ * Created by adit on 4/19/2017.
+ */
+public class LoginApi {
+}
